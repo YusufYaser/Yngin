0477b6c28fe7a30aa84606cd05a79224a45124f5
Yusuf Kelany <me@yusufk.me>
2026-03-12 15:22:11 +0200

    added ImGui and GLFW to the editor

--- Editor/Editor.cpp
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	printf("Editor\n");
-	return 0;
-}
---
+++ Editor/src/Editor.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <Yngin/Yngin.h>
+#include <ImGui/imgui.h>
+#include <ImGui/imgui_impl_opengl3.h>
+#include <ImGui/imgui_impl_glfw.h>
+
+using namespace Yngin;
+
+int main() {
+	printf("Editor\n");
+	return 0;
+}
