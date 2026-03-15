3287fec435cf6b2eaedd21aa7e863048af90ded6
Yusuf Kelany <me@yusufk.me>
2026-03-15 20:07:44 +0200

    Runtime: implement runtime

--- Runtime/src/Runtime.cpp
+++ Runtime/src/Runtime.cpp
@@ -1,6 +1,78 @@
 #include <stdio.h>
+#include <Yngin/Core/Context.h>
+#include <Yngin/Core/Scenes.h>
+#include <Yngin/Rendering/Cameras.h>
+#include <fstream>
+#include <sstream>
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+using namespace Yngin;
+
+void handleCameraMovement(Yngin::Camera* camera);
+
+void error(const char* text) {
+#ifdef _WIN32
+	MessageBoxA(NULL, text, "Yngin Runtime", MB_ICONERROR | MB_OK);
+#else
+	printf(text);
+#endif
+}
 
 int main() {
-	printf("Runtime\n");
+	Yngin::initializeYngin();
+
+	if (!Yngin::isYnginInitialized()) {
+		error("Failed to initialize Yngin\n");
+	}
+
+	std::ifstream scenePak("scene.pak", std::ios::binary);
+	if (!scenePak.is_open()) {
+		error("Failed to open scene.pak");
+		Yngin::terminateYngin();
+		return 1;
+	}
+
+	std::ifstream resourcesPak("resources.pak", std::ios::binary);
+	if (!scenePak.is_open()) {
+		error("Failed to open resources.pak");
+		scenePak.close();
+		Yngin::terminateYngin();
+		return 1;
+	}
+
+	Context* ctx = new Context();
+
+	std::ostringstream resourcesBytes(std::ios::binary);
+	resourcesBytes << resourcesPak.rdbuf();
+	resourcesPak.close();
+	ctx->loadResourcesPak(resourcesBytes.str().c_str(), resourcesBytes.str().size());
+	resourcesBytes.clear();
+
+	std::ostringstream sceneBytes(std::ios::binary);
+	sceneBytes << scenePak.rdbuf();
+	scenePak.close();
+	Scene* scene = ctx->getScenesManager()->createScene(sceneBytes.str().c_str(), sceneBytes.str().size(), 0, true);
+	sceneBytes.clear();
+
+	scene->activate();
+
+	Camera* camera = scene->getCamerasManager()->getCamera(0);
+
+	while (ctx->getStatus() == CONTEXT_STATUS::RUNNING) {
+		handleCameraMovement(camera);
+		ctx->update();
+	}
+
+	Yngin::terminateYngin();
+
 	return 0;
 }
+
+#ifdef _WIN32
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
+	return main();
+}
+#endif
---
+++ Runtime/src/RuntimeCamera.cpp
@@ -0,0 +1,67 @@
+#include <Yngin/Core/Context.h>
+#include <Yngin/Core/InputSystem.h>
+#include <Yngin/Rendering/Cameras.h>
+
+using namespace Yngin;
+
+void handleCameraMovement(Yngin::Camera* camera) {
+	Context* ctx = camera->getContext();
+	InputSystem* input = ctx->getInputSystem();
+
+	double delta = ctx->getDeltaTime();
+
+	ctx->getWindow()->setCursorLocked(input->isMousePressed(MOUSE_BUTTON::RIGHT));
+
+	if (input->isMouseJustPressed(MOUSE_BUTTON::RIGHT)) {
+		input->setMousePosition({ 0, 0 });
+	}
+
+	if (input->isMousePressed(MOUSE_BUTTON::RIGHT)) {
+		glm::vec3 o = camera->getOrientation();
+		glm::ivec2 m = input->getMousePosition(true);
+		input->setMousePosition({ 0, 0 });
+
+		float senstivity = 0.002f;
+
+		float yaw = atan2(o.x, o.y);
+		float pitch = asin(o.z);
+
+		o.x = cos(pitch) * sin(yaw + m.x * senstivity);
+		o.y = cos(pitch) * cos(yaw + m.x * senstivity);
+		o.z = sin(pitch - m.y * senstivity);
+
+		camera->setOrientation(o);
+
+		glm::vec3 forward = glm::normalize(o);
+		glm::vec3 right = glm::normalize(glm::cross(forward, glm::vec3(0, 0, 1)));
+		glm::vec3 realUp = glm::cross(forward, right);
+
+		glm::vec3 change = {};
+
+		if (input->isKeyPressed(Yngin::KEY::W)) {
+			change += forward;
+		}
+		if (input->isKeyPressed(Yngin::KEY::S)) {
+			change -= forward;
+		}
+		if (input->isKeyPressed(Yngin::KEY::D)) {
+			change += right;
+		}
+		if (input->isKeyPressed(Yngin::KEY::A)) {
+			change -= right;
+		}
+		if (input->isKeyPressed(Yngin::KEY::Q)) {
+			change += realUp;
+		}
+		if (input->isKeyPressed(Yngin::KEY::E)) {
+			change -= realUp;
+		}
+
+		float speed = 5.0f;
+		if (input->isKeyPressed(Yngin::KEY::LSHIFT)) {
+			speed *= 1.5f;
+		}
+
+		camera->setPosition(camera->getPosition() + change * (float)delta * speed);
+	}
+}
