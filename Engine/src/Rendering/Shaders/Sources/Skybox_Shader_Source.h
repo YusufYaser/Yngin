52fe3b50a41bafbe0d75040117b28e5500d55cd3
Yusuf Kelany <me@yusufk.me>
2026-04-04 17:16:57 +0200

    Engine: fix skybox black lines when texture filter is linear

--- Engine/src/Rendering/Shaders/Sources/Skybox_Shader_Source.h
+++ Engine/src/Rendering/Shaders/Sources/Skybox_Shader_Source.h
@@ -34,7 +34,24 @@ out vec4 FragColor;
 uniform sampler2D tex0;
 
 void main() {
-	FragColor = texture(tex0, fTexCoord);
+	vec2 localTc = vec2(
+		fTexCoord.x * 4 - floor(fTexCoord.x * 4),
+		fTexCoord.y * 3 - floor(fTexCoord.y * 3)
+	);
+	
+	vec2 tiles = vec2(4.0, 3.0);
+	vec2 tile = vec2(
+		floor(fTexCoord.x * 4),
+		floor(fTexCoord.y * 3)
+	);
+	
+	vec2 texel = 2.0 / (vec2(textureSize(tex0, 0)) / tiles);
+
+	localTc = clamp(localTc, texel, 1.0 - texel);
+	
+	vec2 tc = localTc / tiles + tile / tiles;
+
+	FragColor = texture(tex0, tc);
 }
 			)",
 		};
