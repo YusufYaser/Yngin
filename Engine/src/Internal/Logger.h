cd9acdd138490bd2a2817955394237d8de963fec
Yusuf Kelany <me@yusufk.me>
2026-05-12 17:04:44 +0300

    Add Logger.h

---
+++ Engine/src/Internal/Logger.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define YNGIN_LOGGER_STRINGIZE_DETAIL(x) #x
+#define YNGIN_LOGGER_STRINGIZE(x) YNGIN_LOGGER_STRINGIZE_DETAIL(x)
+
+#ifdef _DEBUG
+#ifdef LOGGER_NAME
+#define DEBUG(fmt, ...) do { \
+printf("[Yngin Debug] [" YNGIN_LOGGER_STRINGIZE(LOGGER_NAME) "] "); \
+printf(fmt "\n", ##__VA_ARGS__); \
+} while (0)
+#else
+#define DEBUG(fmt, ...) do { \
+printf("[Yngin Debug] "); \
+printf(fmt, ##__VA_ARGS__); \
+} while (0)
+#endif
+#else
+#define DEBUG(fmt, ...) ((void)0)
+#endif
